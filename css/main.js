52347c6058376996a8d6e2432e545ecdb2f43c3e
Alexandru Russe <russealexandru@gmail.com>
2023-01-31 11:38:38 +0200

    Add files via upload

--- css/main.js
+++ css/main.js
@@ -9,3 +9,18 @@ function toggleMe() {
 
 }
 
+const open = document.querySelector('#open-modal');
+const closeModalBtn = document.querySelector('#close');
+const modal = document.querySelector('.modal-container');
+// Open modal
+open.addEventListener('click', () => {
+    modal.style.display = 'block';
+// add modal-active class
+    document.body.classList.add('modal-active');
+});
+// Close modal
+closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+// remove modal-active class
+    document.body.classList.remove('modal-active');
+});
